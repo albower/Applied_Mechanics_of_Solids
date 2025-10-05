b92befcd90b84867b18757da3786d69e7cf65315
albower <albower@users.noreply.github.com>
2025-10-05 11:58:33 -0400

    Update Solution8_42.m

--- Solution Codes/Chapter 8/Codes/Solution8_42.m
+++ Solution Codes/Chapter 8/Codes/Solution8_42.m
@@ -4,7 +4,7 @@ function Solution8_42
 %
 %
 %       This code solves problem 8.42 from the text
-%       A.F. Bower 'Solved Problems in Mechanics of Solids'
+%       A.F. Bower 'Solved Problems in Applied Mechanics of Solids'
 %       CRC press, Baton Rouge, 2026
 %
 %       It was downloaded from
@@ -623,6 +623,7 @@ hold on
     scatter3(coord(:,1),coord(:,2),coord(:,3),'MarkerFaceColor',[1,0,0]);
         
     
+
 
 
 
