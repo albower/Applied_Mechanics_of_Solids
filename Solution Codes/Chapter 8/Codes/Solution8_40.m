4275743b02143c03811fbdd7521b125157d2cba2
albower <albower@users.noreply.github.com>
2025-10-05 11:56:59 -0400

    Update Solution8_40.m

--- Solution Codes/Chapter 8/Codes/Solution8_40.m
+++ Solution Codes/Chapter 8/Codes/Solution8_40.m
@@ -4,7 +4,7 @@ function Solution8_40
 %
 %
 %       This code solves problem 8.40 from the text
-%       A.F. Bower 'Solved Problems in Mechanics of Solids'
+%       A.F. Bower 'Solved Problems in Applied Mechanics of Solids'
 %       CRC press, Baton Rouge, 2026
 %
 %       It was downloaded from
@@ -331,3 +331,4 @@ function [N,dNdx,d2Ndx2,Area] = shape(xi,coorda,coordb,coordc)
     Area = Delta/2;
 
 end
+
