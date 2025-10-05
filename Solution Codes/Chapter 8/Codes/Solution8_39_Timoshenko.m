91f5005a7df2f1c5a2b7df31f8b7ae96fd03c516
albower <albower@users.noreply.github.com>
2025-10-05 11:54:24 -0400

    Update Solution8_39_Timoshenko.m

--- Solution Codes/Chapter 8/Codes/Solution8_39_Timoshenko.m
+++ Solution Codes/Chapter 8/Codes/Solution8_39_Timoshenko.m
@@ -3,7 +3,7 @@ function Solution8_39_Timoshenko
 %          Example FEM code using elastic Timoshenko beam elements
 %
 %       This code solves problem 8.39 from the text
-%       A.F. Bower 'Solved Problems in Mechanics of Solids'
+%       A.F. Bower 'Solved Problems in Applied Mechanics of Solids'
 %       CRC press, Baton Rouge, 2026
 %
 %       It was downloaded from
@@ -454,5 +454,6 @@ hold on
     end
 
     scatter3(coord(:,1),coord(:,2),coord(:,3),'MarkerFaceColor',[1,0,0]);
+
 
 end
